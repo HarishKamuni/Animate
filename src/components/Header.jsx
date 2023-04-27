ef21a9849988f4e690d60be073ccf96e4c781f72
Harish Kamuni <108679987+HarishKamuni@users.noreply.github.com>
2023-04-27 10:27:59 +0530

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import Navbar from './Navbar'
 import styled from 'styled-components'
-
+// https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmOQYHSuDgwlcYTW-ZBblTLLtO_kwDFmB0_Q&usqp=CAU
+// https://st2.depositphotos.com/4035913/6124/i/450/depositphotos_61243733-stock-photo-business-company-logo.jpg
 const Header = () => {
   return (
     <>
     <MainHeader>
       <NavLink>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmOQYHSuDgwlcYTW-ZBblTLLtO_kwDFmB0_Q&usqp=CAU" alt='logo' className='logo'/>
+          <img src="https://www.citypng.com/public/uploads/preview/-21602265983dg1gedthiy.png" alt='logo' className='logo'/>
       </NavLink>
       <Navbar/>
     </MainHeader>
@@ -25,9 +26,11 @@ display:flex;
 justify-content: space-between;
 align-items: center;
 
+
 .logo{
-  height:5rem;
-  width:360%;
+  height:10rem;
+  width:20rem;
+  margin-left: 40px;
 }
 `;
 
